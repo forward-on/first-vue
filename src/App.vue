b9dea9de29ecc6e4c98aaa72b91914aba34a63bb
liying <liying@bus365.com>
2018-10-15 17:35:24 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!--<router-view/>-->
+    <h1>{{ msg }}</h1>
+    <firstcomponent></firstcomponent>
+  </div>
+</template>
+
+<script>
+import firstcomponent from './components/FirstComp.vue'
+export default {
+//  name: 'App',
+  data () {
+    return {
+      msg: 'Hello, Vue!'
+    }
+  },
+  components: {firstcomponent}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
